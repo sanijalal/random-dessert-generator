7c46491b38f60306165e2ad4459721c7722da928
uwaseem <u.waseem24@gmail.com>
2020-10-18 17:21:53 +0200

    subbed function keyword to arrow functions

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
 const assert = require('assert')
 const dessertGenerator = require('../index')
 
-describe('dessertGenerator', function() {
-  describe('generateDessert', function() {
-    it('should return a dessert which is a string', function() {
+describe('dessertGenerator', () => {
+  describe('generateDessert', () => {
+    it('should return a dessert which is a string', () => {
       const dessert = dessertGenerator.generateDessert()
       console.info(dessert)
       assert.strictEqual(typeof dessert, 'string')
     })
   })
 
-  describe('generateYummyDessert', function() {
-    it('should return a dessert and adjective which are both string', function() {
+  describe('generateYummyDessert', () => {
+    it('should return a dessert and adjective which are both string', () => {
       const dessert = dessertGenerator.generateYummyDessert()
       console.info(dessert)
       assert.strictEqual(typeof dessert, 'string')
