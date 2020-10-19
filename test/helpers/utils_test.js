2c49c17424bea3dbf6d4917f0dc935b98a93b527
uwaseem <u.waseem24@gmail.com>
2020-10-19 18:17:28 +0200

    fix test files by removing semicolons and spacings

--- test/helpers/utils_test.js
+++ test/helpers/utils_test.js
@@ -4,11 +4,11 @@ const { getRandomIndex } = require('../../helpers/utils')
 describe('helpers', () => {
   describe('getRandomIndex', () => {
     it('should return a randomly index of the array', () => {
-        const arr = ['val1', 'val2', 'val3']
-        const result = getRandomIndex(arr);
+      const arr = ['val1', 'val2', 'val3']
+      const result = getRandomIndex(arr)
 
-        assert.strictEqual(typeof result, 'number')
-        assert.ok(0 <= result && result < arr.length);
+      assert.strictEqual(typeof result, 'number')
+      assert.ok(result >= 0 && result < arr.length)
     })
   })
 })
