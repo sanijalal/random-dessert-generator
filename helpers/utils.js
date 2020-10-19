bca2beaed86662588316de463437d4571e3f3e8e
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-10-19 23:56:34 +0530

    New suggesion on getRandomIndex function

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,5 +1,17 @@
 function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 }
 
 exports.getRandomIndex = getRandomIndex;
+
+/* Hi your getRandomIndex method is not working
+If you want you can use the below method to generate 
+random number between min and max 
+you can pass min as 0 and max as array length 
+Hope this helps
+
+function between(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  */
