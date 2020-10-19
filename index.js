35ae760fea1ab984bca55dc616b0153cdd27317e
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-10-19 23:55:52 +0530

    Import dependencies to index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const utils = require('./helpers/utils');
-
-const desserts = ['ais kacang', 'akok', 'apple crumble', 'brownies', 'buchty', 'cake', 'cekodok', 'cempedak creme', 'cempedak fritters', 'cendol', 'chocolate', 'cupcake', 'eclair', 'frgál', 'kek lapis', 'krapfen', 'kremrole', 'muffin', 'tart', 'větrník']
-const adjectives = ['bbq', 'berrylicious', 'butterscotch', 'caramel', 'cheesy', 'chocolate', 'salted egg', 'salty', 'smoked', 'strawberry', 'sugar', 'sweet']
+const utils = require("./helpers/utils");
+const desserts = require("./data/desserts_list");
+const adjectives = require("./data/adjectives_list");
 
 /**
  * Generate a dessert.
  * @return {string} A random dessert name
  */
 function generateDessert() {
-    const dessertIndex = utils.getRandomIndex(desserts);
-    return desserts[dessertIndex];
+  const dessertIndex = utils.getRandomIndex(desserts);
+  return desserts[dessertIndex];
 }
 
 /**
@@ -17,9 +16,9 @@ function generateDessert() {
  * @return {string} A random dessert name with an adjective
  */
 function generateYummyDessert() {
-    const dessertIndex = utils.getRandomIndex(desserts);
-    const adjectiveIndex = utils.getRandomIndex(adjectives);
-    return `${adjectives[adjectiveIndex]} ${desserts[dessertIndex]}`;
+  const dessertIndex = utils.getRandomIndex(desserts);
+  const adjectiveIndex = utils.getRandomIndex(adjectives);
+  return `${adjectives[adjectiveIndex]} ${desserts[dessertIndex]}`;
 }
 
 exports.generateDessert = generateDessert;
