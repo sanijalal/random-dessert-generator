f362dfd80ebb1a63e62971a51a0ff1322ac2be30
uwaseem <u.waseem24@gmail.com>
2020-10-19 18:36:31 +0200

    added standard mocha env comment to each test files rather in package.json

--- test/helpers/utils_test.js
+++ test/helpers/utils_test.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 const assert = require('assert')
 const { getRandomIndex } = require('../../helpers/utils')
 
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 const assert = require('assert')
 const { generateDessert, generateYummyDessert } = require('../index')
 
